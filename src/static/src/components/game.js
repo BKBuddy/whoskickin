88d4474d4cc67b8c9520443fbbe76b67cd1db270
Brian Kurr <bkurr@onshift.com>
2018-11-05 13:46:11 -0500

    added bootstrap and updated npm;

--- src/static/src/components/game.js
+++ src/static/src/components/game.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import '../stylesheets/game.css';
 
 export default class Game extends Component {
     render(){
-        return (<div>{this.props.home_team}</div>)
+        return (
+            <div className='gameContainer'>
+                <div className={this.props.home_team}>
+                    @{this.props.home_team}
+                </div>
+                <div className={this.props.away_team}>
+                    {this.props.away_team}
+                </div>
+            </div>
+        )
     }
 }
