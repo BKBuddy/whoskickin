df0fe9fa3f72f61e71dba1124357f63fd2d94f97
Brian Kurr <bkurr@onshift.com>
2018-11-26 14:39:16 -0500

    Finally got bootstrap running...;

--- src/static/src/components/App.js
+++ src/static/src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
+import 'jquery';
+import 'popper.js'
+import 'bootstrap';
 import axios from 'axios';
-import '../stylesheets/App.css';
 import Game from './game';
 
 class App extends Component {
@@ -17,14 +19,14 @@ componentDidMount(){
 
   render() {
     return (
-      <div className="App container">
-        <div className='row'>
+      <div className="container">
+        <div className='justify-content-center'>
           {_.keys(this.state).map((key)=>{
-              return <Game key={key} className='justify-content-md-center'
+              return <Game className='row' key={key}
                         home_team={this.state[key]['home_team']}
                         away_team={this.state[key]['away_team']}
                         kicking_team={this.state[key]['kicking_team']}>
-                     </Game>
+                    </Game>
           })}
         </div>
       </div>
--- src/static/src/components/game.js
+++ src/static/src/components/game.js
@@ -5,15 +5,13 @@ import 'bootstrap';
 export default class Game extends Component {
     render(){
         return (
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-sm-4'>
-                        @{this.props.home_team}
-                    </div>
-                    <div className='col-sm-4'></div>
-                    <div className='col-sm-4'>
-                        {this.props.away_team}
-                    </div>
+            <div className='border gameContainer'>
+                <div className='team'>
+                    @{this.props.home_team}
+                </div>
+                <div></div>
+                <div className='team'>
+                    {this.props.away_team}
                 </div>
             </div>
         )
