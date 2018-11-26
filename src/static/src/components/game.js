42066ca79472fcb2d2a8336d8801c45e5455d6ee
Brian Kurr <bkurr@onshift.com>
2018-11-26 15:51:38 -0500

    Added logos directory with all 32 team logos

--- src/static/src/components/game.js
+++ src/static/src/components/game.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import '../stylesheets/game.css';
 import 'bootstrap';
 
+const LOGO_PATH = '../../public/logos';
+
 export default class Game extends Component {
     render(){
         return (
-            <div className='border gameContainer'>
-                <div className='team'>
+            <div className='gameContainer'>
+                <div className='border team'>
+                    <img src={`${LOGO_PATH}/${this.props.home_team}.png`} alt=''></img>
                     @{this.props.home_team}
                 </div>
                 <div></div>
-                <div className='team'>
+                <div className='border team'>
+                    <img src={`${LOGO_PATH}/${this.props.away_team}.png`} alt=''></img>
                     {this.props.away_team}
                 </div>
             </div>
